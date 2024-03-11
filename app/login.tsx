79a004a6b8a0fc26edb7fe64431e177ed5d8da5f
abundance <abundanceanyanwu@gmail.com>
2024-03-11 03:42:10 +0100

    fixed small screen view

--- app/login.tsx
+++ app/login.tsx
@@ -6,6 +6,7 @@ import {
   TextInput,
   Pressable,
   KeyboardAvoidingView,
+  useWindowDimensions,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Feathericons from "@expo/vector-icons/Feather";
@@ -24,7 +25,7 @@ export default function LoginPage() {
     signIn(username, password);
     router.replace("/information");
   }
-
+  const { width, height } = useWindowDimensions();
   return (
     <SafeAreaView className="flex items-center justify-center ">
       <StatusBar style="dark" />
@@ -38,22 +39,26 @@ export default function LoginPage() {
           <Feathericons name="chevron-left" size={40} />
         </Pressable>
       </View>
-      <View className="flex flex-col items-center justify-between h-[90vh] pt-8 px-10 w-full">
+      <View
+        className={`flex flex-col items-center justify-between pt-2 px-10 w-full ${
+          height > 500 ? "h-[90vh] " : "h-[95vh]"
+        }`}
+      >
         <View className="flex space-y-5">
           <SchoolBanner />
           <View className="flex items-center justify-center">
-            <Text className="text-idgray text-xl text-center">COOU</Text>
-            <Text className="text-idgray text-xl text-center">
+            <Text className="text-idgray text-lg text-center">COOU</Text>
+            <Text className="text-idgray text-lg text-center">
               STUDENT E-ID CARD
             </Text>
           </View>
 
           <View className=" w-[80vw] h-[2px]   bg-[#E7E7E7]"></View>
         </View>
-        <View>
-          <Text className="text-xl text-idgray text-center">Student Login</Text>
+        <View className="my-3">
+          <Text className="text-lg text-idgray text-center">Student Login</Text>
 
-          <Text className="font-extralight text-idgray text-center">
+          <Text className="font-extralight text-idgray text-center text-sm">
             Enter your login details below
           </Text>
         </View>
--- components/SchoolBanner.tsx
+++ components/SchoolBanner.tsx
@@ -4,10 +4,10 @@ import { Text, View } from "react-native";
 export default function SchoolBanner() {
   return (
     <View className="flex items-center space-y-2">
-      <View className="flex items-center justify-center w-[230px] h-[230px] border border-idgray rounded-full ">
+      <View className="flex items-center justify-center w-[200px] h-[200px] border border-idgray rounded-full ">
         <Image
           source={"https://i.ibb.co/Tg36KKL/coou-logo-removebg-preview.png"}
-          className="w-[120px] h-[130px]"
+          className="w-[100px] h-[110px]"
         />
       </View>
       <Text className="text-idgray">https://www.coou.edu.ng</Text>
